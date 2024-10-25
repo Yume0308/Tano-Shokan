e6be5195d14e348d694ddd7d3221d04a009d9de0
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-10-26 02:35:36 +0700

    lib
    Add file Font to set to all pages

---
+++ lib/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from "next/font/google";
+
+const FontInter = Inter(
+  {
+    subsets: ["latin"],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  }
+)
+
+export default FontInter;
