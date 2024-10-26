a1f24df11117e6edf60732cc1e93196ebe8f5f5b
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-10-27 00:01:29 +0700

    Frontend
    Add: CardInfo for overview dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,25 @@
+import CardInfo from "@/components/card/card-info";
 import Intro from "@/components/dashboard/intro";
 import ScrollbarContainer from "@/components/dashboard/scrollbar-container";
+import { Card } from "@/components/ui/card";
 
 export default function Page() {
   return (
     <main>
       <ScrollbarContainer scrollable>
-        <div className="space-y-2">
+        <div className="space-y-4">
+          {/* intro  */}
           <Intro />
+
+          {/* cards */}
+          <div className="space-y-4">
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+              <CardInfo />
+              <CardInfo />
+              <CardInfo />
+              <CardInfo />
+            </div>
+          </div>
         </div>
       </ScrollbarContainer>
     </main>
--- components/card/card-info.tsx
+++ components/card/card-info.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image";
 import { Ellipsis } from "lucide-react";
-import { Card } from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 const CardInfo = () => {
   return (
     <>
-      <Card className="p-4 flex-1 min-w-[130px] rounded-2xl">
-        <div className="flex justify-between items-center">
-          <span>
-
-          </span>
-        </div>
+      <Card className="p-1 flex-1 min-w-[130px] rounded-2xl">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-lg font-semibold">Card Title</CardTitle>
+          <Ellipsis />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-medium">
+            1234
+          </div>
+          <h2 className="capitalize text-sm font-medium text-muted-foreground">+20.1% from last month</h2>
+        </CardContent>
       </Card>
     </>
   )
 }
+
+export default CardInfo;
