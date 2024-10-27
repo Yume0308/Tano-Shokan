edb6b8ebe5b5efaddff595fb40a73d617969baf5
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-10-27 21:51:56 +0700

    Update Frontend
    Set position header sticky

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -7,7 +7,7 @@ import ThemeToggle from "../theme/toggle/theme-toggle";
 export function AppHeader() {
   return (
     <>
-      <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear">
+      <header className="flex sticky top-0 h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear border-b z-50">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
--- components/dashboard/intro.tsx
+++ components/dashboard/intro.tsx
@@ -2,7 +2,7 @@ export default function Intro() {
   return (
     <>
       <div className="items-center space-y-1">
-        <div className="text-2xl tracking-tight font-medium block">
+        <div className="text-2xl tracking-tight font-medium">
           Welcome back, <span className="text-orange-200">Shadcn</span> 👋
         </div>
         <div>
